0abc470509c636d6f4807622748ec8f1499f3e15
Rose <enaesorgaldino@gmail.com>
2019-08-14 14:16:20 -0300

    primeiro commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { FormularioComponent } from './componentes/formulario/formulario.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    FormularioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
