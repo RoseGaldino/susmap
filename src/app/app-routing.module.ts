cb8473e97fee20059fae74f408fd4d09e0b4bdd0
Rose <enaesorgaldino@gmail.com>
2019-09-11 13:33:18 -0300

    Foi criado a tela 'Formulario' do projeto susmap e adicionado as rotas do Menu para o fomulário.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {FormularioComponent} from './componentes/formulario/formulario.component';
+import { MenuComponent } from './componentes/menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MenuComponent},
+  {path: 'formulario', component: FormularioComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
